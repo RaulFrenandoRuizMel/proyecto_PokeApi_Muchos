3f81a2717587d2e467f21f28965e6f2a9aca54d2
Raul Ruiz <raulfernandoruizmelendez@gmail.com>
2025-08-19 21:48:06 -0600

    [AGREGAR]avance 3

--- 230309_RAUL_FERNANDO_RUIZ_MELENDEZ/js/conectar_PokeApi.js
+++ 230309_RAUL_FERNANDO_RUIZ_MELENDEZ/js/conectar_PokeApi.js
@@ -6,20 +6,40 @@
 
 const Plantilla_contenedor = document.querySelector(".contenedorPokemon");
 
-const body = document.querySelector("body");
+const main = document.querySelector("main");
 
-for(i=0; i<1024; i++)
+for(i=0; i<1025; i++)
 {
     var clon = Plantilla_contenedor.cloneNode(true);
-    body.appendChild(clon);
+    main.appendChild(clon);
 }
 
 const arreglo_pokemons = document.querySelectorAll(".contenedorPokemon");
 
-for(i=0; i<arreglo_pokemons.length; i++)
-{
 const input_buscar = document.querySelector("input");
 const button_buscar = document.querySelector("button");
+
+button_buscar.addEventListener("click", () =>{
+    const tipo_buscado = input_buscar.value.toLowerCase().trim();
+
+    arreglo_pokemons.forEach(clon =>{
+        if (tipo_buscado === "")
+        {
+            clon.style.display = "block";
+        }
+        else if(clon.classList.contains(tipo_buscado +"1"))
+        {
+            clon.style.display = "block";
+        }
+        else
+        {
+            clon.style.display = "none";
+        }
+    });
+});
+
+for(i=0; i<arreglo_pokemons.length; i++)
+{
 
     fetch("https://pokeapi.co/api/v2/pokemon/"+ (i + 1)).then(recurso => recurso.json()).then(pokemon => {
         
@@ -34,7 +54,7 @@ const button_buscar = document.querySelector("button");
         const imagen_pokemon = arreglo_pokemons[pokemon.id-1].querySelector("img");
         imagen_pokemon.src = pokemon.sprites.front_default;
     
-        const altura_pokemon = arreglo_pokemons[pokemon.id-1].querySelector(".altura_pokemon");
+        const altura_pokemon = arreglo_pokemons[pokemon.id-1].querySelector(".height");
         altura_pokemon.innerHTML = "<p>" + "Altura: " + pokemon.height/10 + "M" + "</p>";
 
         const peso_pokemon = arreglo_pokemons[pokemon.id-1].querySelector(".peso_pokemon");
@@ -56,14 +76,15 @@ const button_buscar = document.querySelector("button");
         const contenedor_audio = arreglo_pokemons[pokemon.id-1].querySelector(".contenedor_audio");
         contenedor_audio.innerHTML = "<audio controls><source src='" + pokemon.cries.latest +"'></audio>";
 
-        const contenedorPokemon = document.querySelector("#contenedorPokemon");
+        const contenedorPokemon = document.querySelector(".contenedorPokemon");
         contenedorPokemon.className = ""; 
         contenedorPokemon.classList.add(pokemon.types[0].type.name + "1");
 
         contenedorPokemon.style.backgroundImage = "url(../230309_RAUL_FERNANDO_RUIZ_MELENDEZ/IMAGENES/"+ pokemon.types[0].type.name +".png)";
 
-        const Separador_Imagen = document.querySelector("#separador_Imagen");
+        const Separador_Imagen = arreglo_pokemons[pokemon.id-1].querySelector(".separador_Imagen");
         Separador_Imagen.style.display = "none"
+        clon.style.display = "none";
     });
 }
 
