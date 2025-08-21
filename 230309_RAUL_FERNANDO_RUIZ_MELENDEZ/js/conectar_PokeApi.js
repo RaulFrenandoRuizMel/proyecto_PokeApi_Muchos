1c6e40cbcdc0eaa647f80d339c7cdd21e272f70c
Raul Ruiz <raulfernandoruizmelendez@gmail.com>
2025-08-20 22:09:26 -0600

    [AGREGAR]avance 4

--- 230309_RAUL_FERNANDO_RUIZ_MELENDEZ/js/conectar_PokeApi.js
+++ 230309_RAUL_FERNANDO_RUIZ_MELENDEZ/js/conectar_PokeApi.js
@@ -8,7 +8,7 @@ const Plantilla_contenedor = document.querySelector(".contenedorPokemon");
 
 const main = document.querySelector("main");
 
-for(i=0; i<1025; i++)
+for(i=0; i<100; i++)
 {
     var clon = Plantilla_contenedor.cloneNode(true);
     main.appendChild(clon);
@@ -38,7 +38,7 @@ button_buscar.addEventListener("click", () =>{
     });
 });
 
-for(i=0; i<arreglo_pokemons.length; i++)
+for(let i=0; i<arreglo_pokemons.length; i++)
 {
 
     fetch("https://pokeapi.co/api/v2/pokemon/"+ (i + 1)).then(recurso => recurso.json()).then(pokemon => {
@@ -75,12 +75,13 @@ for(i=0; i<arreglo_pokemons.length; i++)
         audio_pokemon.src = pokemon.cries.latest; */
         const contenedor_audio = arreglo_pokemons[pokemon.id-1].querySelector(".contenedor_audio");
         contenedor_audio.innerHTML = "<audio controls><source src='" + pokemon.cries.latest +"'></audio>";
-
-        const contenedorPokemon = document.querySelector(".contenedorPokemon");
+        
+        const contenedorPokemon = arreglo_pokemons[pokemon.id - 1];
         contenedorPokemon.className = ""; 
         contenedorPokemon.classList.add(pokemon.types[0].type.name + "1");
 
         contenedorPokemon.style.backgroundImage = "url(../230309_RAUL_FERNANDO_RUIZ_MELENDEZ/IMAGENES/"+ pokemon.types[0].type.name +".png)";
+
 
         const Separador_Imagen = arreglo_pokemons[pokemon.id-1].querySelector(".separador_Imagen");
         Separador_Imagen.style.display = "none"
